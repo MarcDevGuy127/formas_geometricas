76cbd81da102d6472216556e498ca590bf5023da
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 18:13:16 -0300

    chore(Quadrado): extend Retangulo in Quadrado class

--- formas_geometricas/src/entities/Quadrado.java
+++ formas_geometricas/src/entities/Quadrado.java
@@ -1,5 +1,9 @@
 package entities;
 
-public class Quadrado {
-
+public class Quadrado extends Retangulo {
+	
+	public Quadrado(double altura, double largura) {
+		super(altura, largura);
+		// TODO Auto-generated constructor stub
+	}
 }
