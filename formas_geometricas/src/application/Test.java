81a3f1fd11f12d0d6994947dbd7b669dbf2fb464
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 19:13:09 -0300

    chore(Triangulo): update class Triangulo and methods

--- formas_geometricas/src/application/Test.java
+++ formas_geometricas/src/application/Test.java
@@ -3,15 +3,18 @@ package application;
 import entities.Circulo;
 import entities.Quadrado;
 import entities.Retangulo;
+import entities.Triangulo;
 
 public class Test {
 
 	public static void main(String[] args) {
 		Forma c = new Circulo(5.0);
 		Forma r = new Retangulo(5.0,2.0);
+		Forma t = new Triangulo(4.0,2.0);
 		Forma q = new Quadrado(10.0,2.0);
 		System.out.println("Area Circulo: " + c.calcularArea());
 		System.out.println("Area Retangulo: " + r.calcularArea());
+		System.out.println("Area Triangulo: " + t.calcularArea());
 		System.out.println("Area Quadrado: " + q.calcularArea());
 	}
 
--- formas_geometricas/src/entities/Triangulo.java
+++ formas_geometricas/src/entities/Triangulo.java
@@ -1,5 +1,19 @@
 package entities;
 
-public class Triangulo {
+import application.Forma;
 
+public class Triangulo extends Forma {
+	double base, altura;
+
+	public Triangulo(double base, double altura) {
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	@Override
+	public double calcularArea() {
+		return (base * altura)/ 2;
+	}
+	
+	
 }
