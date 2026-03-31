3e5874d994314e0395b392785be9b170c71941c5
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 18:13:42 -0300

    test(Test): add Quadrado test in Forma

--- formas_geometricas/src/application/Test.java
+++ formas_geometricas/src/application/Test.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.Circulo;
+import entities.Quadrado;
 import entities.Retangulo;
 
 public class Test {
@@ -8,8 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		Forma c = new Circulo(5.0);
 		Forma r = new Retangulo(5.0,2.0);
+		Forma q = new Quadrado(10.0,2.0);
 		System.out.println("Area Circulo: " + c.calcularArea());
 		System.out.println("Area Retangulo: " + r.calcularArea());
+		System.out.println("Area Quadrado: " + q.calcularArea());
 	}
 
 }
